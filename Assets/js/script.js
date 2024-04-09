bda2f32efe441eafd0557e9941bba4d8d32a4433
Kelechi Ekwulugo <kekwulugo@gmail.com>
2024-04-09 17:20:29 -0600

    Remove gamestart method

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -14,32 +14,23 @@ const gameBoard = (() => {
 
 
 //factory function that creates player object
-
-
-
-const createPlayer = (name, symbol)=> {
+const createPlayer = (name, symbol, moves)=> {
 
  return {
   name,
-  symbol
+  symbol,
+  moves
  }
 };
 
 const gamePlay = (() =>{
 
- //array of players
- 
-
- //track current player
-
-
- // start game function
-const gameStart = () => {
-
  //prompt for player 1 name
  let player1 = prompt("Player 1?")
  let player2 = prompt("Player 2?")
- alert("Player 1 goes first and will be X")
+ alert(player1 +" goes first and will be X")
+
+ 
 
  //create players
  const players = [createPlayer(player1, "X"),createPlayer(player2, "O")];
@@ -47,34 +38,15 @@ const gameStart = () => {
  //track current player index - player X always goes first
  const currentPlayerIndex = 0;
 
- 
 
  return {
   players,
   currentPlayerIndex
  }
 
-}
-
-
-
-  
-
-// handle game click ( what happens when player chooses a cell?)
- // switch between player one symbol and player two symbol
- // check if cell is already occupied otherwise add symbol in cell
-
-//return so that it can used
-return {
- gameStart,
-
-}
-
-
-
 })();
 
-gamePlay.gameStart();
+
 
 
 
