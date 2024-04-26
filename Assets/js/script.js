a4c932e9073706b40316272f33ab10d8f154ac66
Kelechi Ekwulugo <kekwulugo@gmail.com>
2024-04-25 22:38:54 -0600

    Add refactored code

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,65 +1,87 @@
-const gameBoard = (() => {
- const board = ["","","","","","","","",""]
-
-
- //render board + create a square with an index
-
- // you need to return so that it can be accessed by other modules
- return {
-  board
- }
-
-})();
-
-
-
-//factory function that creates player object
-const createPlayer = (name, symbol, moves)=> {
-
- return {
-  name,
-  symbol,
-  moves
- }
-};
-
-const gamePlay = (() =>{
-
- //prompt for player 1 name
- let player1 = prompt("Player 1?")
- let player2 = prompt("Player 2?")
- alert(player1 +" goes first and will be X")
-
- 
-
- //create players
- const players = [createPlayer(player1, "X"),createPlayer(player2, "O")];
-
- //track current player index - player X always goes first
- const currentPlayerIndex = 0;
-
-
- return {
-  players,
-  currentPlayerIndex
- }
-
-})();
-
-
-
-
-
-
-
-
-
-
+const gameBoard = (()=>{
   
+  const board = ["X","X","X","","","","","","",""];
+  
+  return {
+    board
+  };
+   
+  
+})();
 
+const createPlayer = (name, symbol) => {
+  return{
+    name,
+    symbol
+};
+  
+}
 
-
-
-
-
-
+const gamePlay = (() => {
+  
+  //setup our players - this will be controlled by the DOM 
+  let player1 = createPlayer("Player 1", "X");
+  let player2 = createPlayer("Player 2", "O");
+    
+  //set player counter and turn manager
+  let player1Turn = true;
+  
+  const playerToggle = () =>{
+    player1Turn = !player1Turn;
+  }
+  
+  const checkWinner = () =>{
+    let currentBoard = gameBoard.board;
+    let winConditions = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8], 
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6]
+    ];
+    
+    for (let combo of winConditions){
+      const [a,b,c] = combo;
+      if (currentBoard[a]!="" && currentBoard[a] ===currentBoard[b] && currentBoard[a] ===currentBoard[c]){
+        return a;
+      } else if (currentBoard.every(item => item!="")){
+        return a;
+      } else{
+        return false;
+      }
+    }
+   
+}
+  
+  const play = (symbol) =>{
+    //check player turn
+    let index = prompt ("Where would you like to place your symbol?");
+    console.log(gameBoard.board);
+    if (gameBoard.board[index]==""){
+      gameBoard.board[index] = symbol;
+      playerToggle();
+      alert(gameBoard.board)
+      takeTurn();      
+    } 
+    alert("There is already a symbol here");
+  }
+  const takeTurn = () => {
+    console.log(checkWinner());
+     
+    if (checkWinner()){
+      console.log("End of Game");
+    } else{
+      if (player1Turn){
+      let player = player1;
+        play(player.symbol);
+    } else {
+      let player = player2;
+      play(player.symbol);
+    }
+    };        
+  }
+  takeTurn();
+})();
