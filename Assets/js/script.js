176dd9e3b054846d83b944f22b8c8998a1cdddbb
Kelechi Ekwulugo <kekwulugo@gmail.com>
2024-04-30 21:10:13 -0600

    Update UI and game logic

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,12 +1,16 @@
 //creates DOM object to update UI
 const DOM = (() => {
-  let startBtn = document.querySelector("button");
+  let startBtn = document.querySelector(".start");
   let gridContainer = document.querySelector(".board-container");
-  
+  let gameStatus = document.querySelector(".game-status");
+  let wrapper = document.querySelector(".wrapper");
+
 
   return {
     startBtn,
     gridContainer,
+    gameStatus,
+    wrapper
 
   };
 })();
@@ -33,10 +37,6 @@ const gameBoard = (() => {
   };
 })();
 
-
-
-
-
 const createPlayer = (name, symbol) => {
   return {
     name,
@@ -45,9 +45,23 @@ const createPlayer = (name, symbol) => {
 };
 
 const gameController = (() => {
-  //setup our players - this will be controlled by the DOM
-  let player1 = createPlayer("Player 1", "X");
-  let player2 = createPlayer("Player 2", "O");
+  
+  const getPlayers = ()=>{
+    let player1name = prompt("Player 1, please enter your name");
+    let player2name = prompt("Player 2, please enter your name here");
+
+    let player1 = createPlayer(player1name, "X");
+    let player2 = createPlayer(player2name, "O");
+
+    alert("Click start game to continue!")
+
+    return {
+      player1,
+      player2
+    }
+  }
+
+  const {player1, player2} = getPlayers();
 
   //set player counter and turn manager
   let player1Turn = true;
@@ -70,6 +84,7 @@ const gameController = (() => {
     ];
 
     for (let combo of winConditions) {
+  
       const [a, b, c] = combo;
       if (
         currentBoard[a] != "" &&
@@ -80,19 +95,30 @@ const gameController = (() => {
       }
     }
     if (currentBoard.every((item) => item != "")) {
-      return true;
+      return "Tie";
     }
 
     return false;
   };
 
+  const resetGame = () =>{
+    let resetBtn = document.createElement("button");
+    resetBtn.innerText = "Reset Game";
+    resetBtn.addEventListener('click', ()=>{
+      location.reload();
+    });
+    DOM.wrapper.append(resetBtn);
+
+
+
+  }
+
   const play = (symbol) => { 
 
     let gridItems = document.querySelectorAll(".grid-item");
-
     gridItems.forEach((item)=> item.addEventListener('click', function (event){
-      let index = event.target.dataset.index;
-        console.log(gameBoard.board[index]);
+    
+    let index = event.target.dataset.index;
 
         if (gameBoard.board[index] === "") {
 
@@ -104,40 +130,41 @@ const gameController = (() => {
         } else {
           alert("There is already a symbol here");
         } 
-
-
     }))
   };
 
   const takeTurn = () => {
-    let gameEnd = checkWinner();
-    console.log(gameEnd);
-    
-    let player;
-    console.log(player1Turn);
 
-    if (gameEnd) {
-      console.log("End of Game");
+    DOM.startBtn.classList.add("hidden");
+    let gameEnd = checkWinner();
+    let player;
+
+    if (gameEnd === "Tie") {
+      DOM.gameStatus.innerHTML = `It is a tie!`;
+      resetGame();
+      
+
+    } else if (gameEnd) {
+      player1Turn? DOM.gameStatus.innerHTML = `${player2.name} Wins!` : DOM.gameStatus.innerHTML = `${player1.name} Wins!`;
+      resetGame();
+       
 
     } else {
-
       if (player1Turn) {
         player = player1;
-        console.log(player.symbol)
         play(player.symbol);
+        DOM.gameStatus.innerHTML = `${player.name}'s turn`;
 
       } else {
         player = player2;
-        console.log(player.symbol)
         play(player.symbol);
+        DOM.gameStatus.innerHTML = `${player.name}'s turn`;
       }
     }
   };
 
   //Testing DOM Elements
-
   const startGame = () => {
-    console.log("game to start");
     gameBoard.renderBoard();
     takeTurn();
   };
